07fd2c30c20bfea87c7ee7624948fa6091b90c85
Janjirala Bhargavi <janjiralabhargavi9391@gmail.com>
2024-05-03 13:29:38 +0530

    added env variables

--- db.js
+++ db.js
@@ -1,10 +1,17 @@
 const Sequelize = require("sequelize");
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Create a Sequelize instance with connection parameters
-const sequelize = new Sequelize("blog", "postgres", "root", {
-  host: "localhost",
-  dialect: "postgres", // Specify the dialect
-});
+const sequelize = new Sequelize(
+  "blog",
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "postgres", // Specify the dialect
+  }
+);
 
 // Test the connection
 sequelize
